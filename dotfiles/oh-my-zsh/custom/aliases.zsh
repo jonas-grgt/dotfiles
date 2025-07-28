d61238467f40736e897107ace8fc1619b63ce87e
JonasG <jonas.grgt@gmail.com>
2025-07-28 23:15:59 +0200

    feat(zsh): add opi and zz aliases

--- dotfiles/oh-my-zsh/custom/aliases.zsh
+++ dotfiles/oh-my-zsh/custom/aliases.zsh
@@ -4,4 +4,6 @@ alias vim=nvim
 alias dtd=dotdrop
 alias glola='git log --graph --pretty="%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset (signed: %C(bold yellow)%G?%Creset)" --all'
 alias gho='open_command "$(git config --get remote.origin.url | sed -e "s#git@.*\.*github\.com:#https://github.com/#" -e "s/\.git$//")"'
+alias opi='omz plugin info'
+alias zz=zellij
 
